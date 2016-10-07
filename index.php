60c11b602ebe3e5410525c770627b48acf7eb366
eromero <edu88_1@hotmail.com>
2016-10-07 14:21:03 -0400

    modificando el index

--- index.php
+++ index.php
@@ -307,7 +307,7 @@ switch (ENVIRONMENT)
 
 /*
  * --------------------------------------------------------------------
- * LOAD THE BOOTSTRAP FILE
+ * LOAD THE BOOTSTRAP FILE sfsdgdf
  * --------------------------------------------------------------------
  *
  * And away we go...
